05f04960f8241621c877f8c52159bf1b57f37068
Nawal <maltego432@gmail.com>
2023-10-29 14:07:38 +0300

    Add project files.

---
+++ Core/Entities/Address.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Core.Entities
+{
+    public class Address : EntityBase
+        {
+            public String Street { get; set; }
+            public string City { get; set; }
+            public int Number { get; set; }
+        }
+}
---
+++ Core/Entities/EntityBase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+   public class EntityBase
+    {
+        public Guid Id { get; set; }// Guid = the best for Projects
+    }
+}
